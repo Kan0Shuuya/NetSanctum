9029dc5113c39a992321951def835dc838d107fb
Kan0Shuuya <nastasya.ustinskaya@bk.ru>
2025-04-27 10:34:12 +0300

    create auth html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
@@ -7,6 +7,6 @@ def create_app():
     
     @app.route('/')
     def main_page():
-        return 'HELLO, MAIN PAGE'
+        return render_template('index.html')
     
     return app
