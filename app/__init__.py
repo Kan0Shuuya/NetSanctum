fe039481134f05a513a2a5b6fb0642fd3fa110ff
Kan0Shuuya <nastasya.ustinskaya@bk.ru>
2025-04-28 08:44:45 +0300

    realese flask-module

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,39 @@
+import importlib
+import json
 import os
 from flask import Flask, abort, redirect, render_template, current_app, request, session, url_for
-import config
-import auth.routes
+
 
 def create_app():
     app = Flask(__name__)
-    from .auth import auth as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-    app.config.from_object(config.Config)
+    modules_dir = os.path.join(os.path.dirname(__file__), 'modules')
+    for module_folder in os.listdir(modules_dir):
+        full_path = os.path.join(modules_dir, module_folder)
+        if os.path.isdir(full_path):
+            info_json = os.path.join(full_path, "info.json")
+            if os.path.exists(info_json):
+                print("Import {info_json}...")
+                with open(info_json, encoding="utf-8") as file:
+                    meta = json.load(file)
+                    py_mod = importlib.import_module(f"app.modules.{module_folder}")
+                    if hasattr(py_mod, 'bp'):
+                        app.register_blueprint(py_mod.bp, url_prefix=meta.get('route_prefix'))
+                    else:
+                        print("EROR! not found bp attr in module!")                    
+                    
+            else:
+                print(f"EROR! can't export {info_json}")
+
+                
+                
+        
+        else:
+            print(f"EROR! {full_path} is not dir")            
+            
+    
     
     @app.route('/')
-    @auth.routes.my_login_required()
     def index():
-       return render_template('workspace.html', user=session['user'])
+       return render_template('workspace.html')
         
     return app
